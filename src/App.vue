f38da83f49293030f8eda3527aa4409b37de3da0
小李不见了 <11722237+xiao-li-is-missing@user.noreply.gitee.com>
2023-04-07 15:23:19 +0800

    feat:(配置prettier插件)

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,10 @@
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+<script setup>
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
 </script>
 
 <style lang="scss">
